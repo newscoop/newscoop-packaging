3c3782ce3ad713c5bf7513e5fe6fdd6cc93a40c0
Daniel James <daniel@64studio.com>
2014-07-08 12:49:21 +0100

    We are now building for Fedora 20

--- gen-rpm.sh
+++ gen-rpm.sh
@@ -22,7 +22,7 @@ echo -n "OK? [enter|CTRL-C]" ; read
 cp -r ./rpm /tmp/newscoop-${VERSION}/
 cd ${TMP}/
 
-# workarounds for Newscoop 4.2.2 spaces in filenames, symbolic links etc.
+# workarounds for Newscoop 4.2.3 spaces in filenames, symbolic links etc.
 mv newscoop-${VERSION}/newscoop/vendor/symfony/symfony/src/Symfony/Component/Finder/Tests/Fixtures/with\ space/ newscoop-${VERSION}/newscoop/vendor/symfony/symfony/src/Symfony/Component/Finder/Tests/Fixtures/with_space/
 
 # end workarounds
@@ -45,21 +45,21 @@ echo -n "UPLOAD? [enter|CTRL-C]" ; read
 YUMSIG=$(grep -v "^#" ~/.rpmmacros  | grep "_gpg_name  Sourcefabric")
 
 if [ -z "$YUMSIG" ]; then
-	YUMHOST=rg42.org
+	YUMHOST=yum.example.com
 	YUMPATH=/var/www/yum
 else
 	YUMHOST=yum.sourcefabric.org
-	YUMPATH=/home/rgareus/yum
+	YUMPATH=/var/www/yum
 fi
 
-rsync -P --bwlimit=70 ${HOME}/rpmbuild/RPMS/noarch/newscoop-${RPMVERS}${RPMRELEASE}.noarch.rpm ${YUMHOST}:${YUMPATH}/18/i386/ || exit
-rsync -P --bwlimit=70 ${HOME}/rpmbuild/SRPMS/newscoop-${RPMVERS}${RPMRELEASE}.src.rpm ${YUMHOST}:${YUMPATH}/18/source/ || exit
+rsync -P --bwlimit=70 ${HOME}/rpmbuild/RPMS/noarch/newscoop-${RPMVERS}${RPMRELEASE}.noarch.rpm ${YUMHOST}:${YUMPATH}/20/i386/ || exit
+rsync -P --bwlimit=70 ${HOME}/rpmbuild/SRPMS/newscoop-${RPMVERS}${RPMRELEASE}.src.rpm ${YUMHOST}:${YUMPATH}/20/source/ || exit
 
 ssh ${YUMHOST} << EOF
-cd ${YUMPATH}/18/x86_64/
+cd ${YUMPATH}/20/x86_64/
 ln ../i386/newscoop-${RPMVERS}${RPMRELEASE}.noarch.rpm
 
-createrepo ${YUMPATH}/18/source/
-createrepo ${YUMPATH}/18/i386/
-createrepo ${YUMPATH}/18/x86_64/
+createrepo ${YUMPATH}/20/source/
+createrepo ${YUMPATH}/20/i386/
+createrepo ${YUMPATH}/20/x86_64/
 EOF
